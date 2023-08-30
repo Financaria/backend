1e5bd2103c010f6513cd74fe796a43b4129ceabc
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-08-30 20:54:47 -0300

    Conexão do login com o banco de dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
+import type { respostaPadrao } from "../../types/respostaPadrao"
 
-export default (
+const endpointLogin = (
     req : NextApiRequest, 
-    res: NextApiResponse
+    res : NextApiResponse<respostaPadrao>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
         if(login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-                res.status(200).json({msg : 'Usuário autenticado com sucesso.'});
+                return res.status(200).json({msg : 'Usuário autenticado com sucesso.'});
             }
             return res.status(400).json({msg : 'Usuário ou senha não encontrados.'});
     }
-    return res.status(405).json({erro : 'Método informado não é válido.'});
+    return res.status(405).json({error : 'Método informado não é válido.'});
 }
+
+export default conectarMongoDB(endpointLogin);
