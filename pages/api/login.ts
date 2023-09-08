677644daecbc957865ee69f9abb39be06830e6d4
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-08 19:35:15 -0300

    Adiciona o Model no endpoint login e busca o usuário no banco

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
 import type { respostaPadrao } from "../../types/respostaPadrao"
+import md5 from "md5";
+import { UsuarioModel } from "../../models/UsuarioModel";
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest, 
     res : NextApiResponse<respostaPadrao>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'Admin@123'){
-                return res.status(200).json({msg : 'Usuário autenticado com sucesso.'});
+        // checar se o usuário existe no banco
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+                return res.status(200).json({msg : `Usuário ${usuarioEncontrado.nome} autenticado com sucesso.`});
             }
             return res.status(400).json({msg : 'Usuário ou senha não encontrados.'});
     }
