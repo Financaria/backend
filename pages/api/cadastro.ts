4cd6e7bd542ab65706d144a68848836b2154cf05
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-08 19:17:24 -0300

    Valida a duplicidade de usuário

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB"
 import type { respostaPadrao } from "../../types/respostaPadrao";
 import type { cadastroRequisicao } from "../../types/cadastroRequisicao";
 import { UsuarioModel } from "../../models/UsuarioModel"
+import md5 from "md5";
 
 const endpointCadastro = async (
     req : NextApiRequest,
@@ -25,10 +27,22 @@ const endpointCadastro = async (
             return res.status(400).json({error : 'Senha inválida.'});
         }
 
-        await UsuarioModel.create(usuario);
+        // validação de email (verfica se já existe usuario com o email informado)
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if (usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({error : 'Já existe uma conta com o email informado.'});
+        }
+
+        // salvar usuário no banco de dados
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuário criado com sucesso.'})
     }
     return res.status(405).json({error : 'Método informado não é válido.'})
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
