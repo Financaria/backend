5e2e0ae818071375e715711c2a24599d14a896d9
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-12 13:50:14 -0300

    Update UsuarioModel.ts

--- models/UsuarioModel.ts
+++ models/UsuarioModel.ts
@@ -4,7 +4,7 @@ const UsuarioSchema = new Schema({
     nome: {type : String, required : true},
     email: {type : String, required : true},
     senha: {type : String, required : true},
-    despesas: {type : Array, required : true, default : []},
+    saldo: {type: Number, default : 0}
 });
 
 export const UsuarioModel = (mongoose.models.users || 
