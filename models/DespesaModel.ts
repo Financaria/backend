93d96816959f1bfa33e8002efe445608fa548079
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-15 18:56:35 -0300

    Cria o Schema e o Model de Despesa

---
+++ models/DespesaModel.ts
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose";
+
+const DespesaSchema = new Schema({
+    idUsuario : {type : String, required : true},
+    descricao: {type : String, required : true},
+    categoria: {type : String, required: true},
+    valor: {type : Number, required: true},
+    dataVencimento : {type : Date, required : true},
+    dataPagamento : {type : Date, required : true},
+    dataInclusao : {type : Date, required : true},
+    parcelas : {type : Number, required : true}, // default : 0
+    recorrencia : {type : Boolean, required : true} // default : false
+});
+
+export const DespesaModel = (mongoose.models.despesas || 
+    mongoose.model('despesas', DespesaSchema));
