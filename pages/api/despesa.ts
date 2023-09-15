86cd6d561fb3d510aa0d20f263ad05a65fc05c38
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-15 18:57:22 -0300

    Salva a despesa no banco de dados

--- pages/api/despesa.ts
+++ pages/api/despesa.ts
@@ -5,7 +5,7 @@ import { DespesaModel } from '../../models/DespesaModel';
 import { UsuarioModel } from '../../models/UsuarioModel';
 // import { validarToken } from '../../middlewares/';
 
-const endpointDespesa = (
+const endpointDespesa = async (
     req : NextApiRequest,
     res : NextApiResponse<respostaPadrao>
 ) => {
@@ -13,29 +13,61 @@ const endpointDespesa = (
     if(req.method === 'POST'){
         // pegar os dados do usuário logado  -  como eu pego os dados do usuario logado?
 
-        const {descricao, valor, dataVencimento, parcelas, recorrencia, categoria} = req.body;
+        try{
+            // const {userId} = req.query;
 
-        if(!req || !req.body){
-            return res.status(400).json({error : 'Parâmetros de entrada não informados.'});
+            const {idUsuario, descricao, categoria, valor, dataVencimento, dataPagamento, parcelas, recorrencia} = req.body;
+            
+            const usuario = await UsuarioModel.findById(idUsuario);   // ({email : login, senha : md5(senha)})
+            if(!usuario){
+                return res.status(400).json({error : 'Usuário não encontrado.'});
+            }
+
+            
+
+            if(!req || !req.body){
+                return res.status(400).json({error : 'Parâmetros de entrada não informados.'});
+            }
+
+            if(!descricao || descricao.length < 2){
+                return res.status(400).json({error : 'Descrição não é válida.'});
+            }
+
+            if(!valor){
+                return res.status(400).json({error : 'É necessário informar um valor.'});
+            }
+
+            if(!dataVencimento){
+                return res.status(400).json({error : 'É necessário informar uma data.'});
+            }
+
+            if(!dataPagamento){
+                dataPagamento : dataVencimento;
+                console.log('dataVencimento', dataVencimento)
+            }
+
+            const despesa = {
+                // idUsuario : usuario._id,
+                idUsuario,
+                descricao,
+                categoria,
+                valor,
+                dataVencimento,
+                dataPagamento : dataVencimento,
+                dataInclusao : new Date(),
+                parcelas,
+                recorrencia    
+            }
+
+            await DespesaModel.create(despesa)
+            return res.status(200).json({msg : 'Despesa criada com sucesso.'});
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({error: 'Erro ao cadastrar despesa.'});
         }
 
-        if(!descricao || descricao.length < 2){
-            return res.status(400).json({error : 'Descrição não é válida.'});
-        }
-
-        if(!valor){
-            return res.status(400).json({error : 'É necessário informar um valor.'});
-        }
-
-        if(!dataVencimento){
-            return res.status(400).json({error : 'É necessário informar uma data.'});
-        }
-
-        return res.status(200).json({msg : 'Despesa está válida.'});
-
-    }else{
-        return res.status(405).json({error : 'Método informado não é válido.'});
     }
+
 }
 
 export default conectarMongoDB(endpointDespesa);
