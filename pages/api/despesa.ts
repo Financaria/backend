d2578d328aaf1cdaa479a93e79bec5eae02c5cf7
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-05 20:17:19 -0300

    PUT de despesas - parte 1

--- pages/api/despesa.ts
+++ pages/api/despesa.ts
@@ -8,7 +8,6 @@ import nc from "next-connect";
 import { CORSPolicy } from './../../middlewares/CORSpolicy';
 
 
-
 const handler = nc()
     .post(async (req : NextApiRequest, res : NextApiResponse<respostaPadrao | any>) => {
         try{
@@ -19,9 +18,14 @@ const handler = nc()
                 return res.status(400).json({error : 'Usuário não encontrado.'});
             }
 
+            //console.log(req.body)
+            //console.log('categoria', req?.body?.categoria)
+            console.log('req', req)
+
             if(!req || !req.body){
-                return res.status(400).json({error : 'Parâmetros de entrada não informados.'});
+                                return res.status(400).json({error : 'Parâmetros de entrada não informados.'});
             }
+
             const {descricao, categoria, valor, dataVencimento, dataPagamento, parcelas, recorrencia} = req?.body;
 
             if(!descricao || descricao.length < 2){
@@ -61,27 +65,25 @@ const handler = nc()
     .get(async (req : NextApiRequest, res : NextApiResponse<respostaPadrao | any[]>) => {
         try{
             const {userId} = req?.query;
+            const usuario = await UsuarioModel.findById(userId);
+            console.log('usuario', usuario);
 
-            //falta incluir a lógica para pegar apenas as despesas do mês escolhido.
+            if(!usuario){
+                return res.status(400).json({error: 'Usuário não encontrado.'});
+            }
 
             const {filtro} = req.query;
+            const anoAlvo = 2023;
+            const mesAlvo = parseInt(filtro);
+            
             if(!filtro || filtro.length < 2){
                 const despesas = await DespesaModel.find({idUsuario: userId})
                 .sort({dataInclusao : -1});;
                 return res.status(200).json(despesas);
             }
-
-            const anoAlvo = 2023;
-            // const mesAlvo = parseInt(filtro[0]);
-            const mesAlvo = parseInt(filtro);
-            // const diaAlvo = parseInt(filtro);
-            console.log(filtro)
-            console.log(typeof filtro)
-            console.log(mesAlvo)
-            console.log(typeof mesAlvo)
-
+            
             const despesasEncontradas = await DespesaModel.find({
-                $or: [{ descricao: {$regex: filtro, $options : 'i'}},
+                $or: [{ descricao: {$regex : filtro, $options : 'i'}},
                     { categoria: {$regex: filtro, $options : 'i'}},
                     { 
                         $expr: {
@@ -102,15 +104,74 @@ const handler = nc()
                     },
                     { dataVencimento: filtro },
                     { dataPagamento: filtro }
-                ]
+                ],
+                $and: [{idUsuario : usuario._id}]
             });
             return res.status(200).json(despesasEncontradas);
 
         }catch(e){
             console.log(e);
+            return res.status(400).json({error : 'Não foi possível obter dados.'})
         }
 
-        return res.status(400).json({error : 'Não foi possível obter dados.'})
+    })
+    .put(async(req : NextApiRequest, res : NextApiResponse<respostaPadrao> | any) => {
+        try{
+            const {id} = req?.query;
+            //const usuario = await UsuarioModel.findById(userId);
+            const despesa = await DespesaModel.findById(id);
+            //.findOne(_idUsuario : userId && )
+
+            if(!despesa){
+                return res.status(400).json({error: 'Despesa não encontrada.'});
+            }
+
+            const {descricao} = req.body;
+            console.log(descricao);
+            if(descricao && descricao > 2){
+                despesa.descricao = descricao;
+                //console.log(descricao);
+
+            }
+
+            const {categoria} = req.body;
+            if(categoria && categoria > 2){
+                despesa.categoria = categoria;
+            }
+
+            const {valor} = req.body;
+            if(valor){
+                despesa.valor = valor;
+            }
+
+            const {dataVencimento} = req.body;
+            if(dataVencimento){
+                despesa.dataVencimento = dataVencimento;
+            }
+
+            const {dataPagamento} = req.body;
+            if(dataPagamento){
+                despesa.dataPagamento = dataPagamento;
+            }
+
+            return res.status(200).json({despesa})
+
+            await DespesaModel
+                .findByIdAndUpdate({_id : despesa._id}, despesa);
+            return res.status(200).json({msg: 'Despesa alterada com sucesso.'});
+            //console.log('despesa atualizada', despesa)
+
+
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({error : 'Não foi possível atualizar a despesa.'})
+        }
     });
+
+export const config = {
+    api : {
+        bodyParser : false
+    }
+}
 
 export default CORSPolicy(validarToken(conectarMongoDB(handler)));
