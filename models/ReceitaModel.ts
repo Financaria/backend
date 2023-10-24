3869c020f734a05025ab15d2b4d20e0c00e30bcb
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-24 17:22:18 -0300

    Ajusta o cálculo do saldo no POST das Receitas

--- models/ReceitaModel.ts
+++ models/ReceitaModel.ts
@@ -7,6 +7,7 @@ const ReceitaSchema = new mongoose.Schema({
     valor : {type : Number, required : true},
     dataInclusao : {type : Date, required : true},
     dataRecebimento : {type : Date, required : true},
+    recebido: {type : Boolean, required : true},
     parcelas : {type : Number, required : false, default : 1}, 
     recorrencia : {type : Boolean, required : false, default : false} 
 });
